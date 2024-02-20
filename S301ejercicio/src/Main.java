fd93b8db8102cf99ac3f770742f04571b909739c
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-02-20 17:03:47 +0100

    Update Main.java

--- S301ejercicio/src/Main.java
+++ S301ejercicio/src/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
             Undo undoVariable1 = Undo.getInstance();
-          
+            Undo undoVariable2 = Undo.getInstance();
+
+
             undoVariable1.createUndo("Pedido 1");
-            undoVariable1.createUndo("Pedido 2");
+            undoVariable2.createUndo("Pedido 2");
             undoVariable1.createUndo("Pedido 3");
-            undoVariable1.createUndo("Pedido 4");
+            undoVariable2.createUndo("Pedido 4");
             undoVariable1.createUndo("Pedido 5");
 
             undoVariable1.printUndo();
